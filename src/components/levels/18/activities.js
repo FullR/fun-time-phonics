1632d87d66dc0097ac08c49a649c52d7ee8ff439
James Meyers <James.Meyers919@gmail.com>
2015-11-17 11:04:50 -0800

    Reviewed level 18

--- src/components/levels/18/activities.js
+++ src/components/levels/18/activities.js
@@ -3,25 +3,25 @@ import Activity from "./activity";
 import soundContext from "decorators/sound-context";
 
 export default [
-  {words: ["in", "on", "end"], correct: "in"},                  // 1
-  {words: ["bug", "beg", "big"], correct: "big", wordsOnly: true},
-  {words: ["fin", "fan", "fun"], correct: "fin", wordsOnly: true},
-  {words: ["pin", "pen", "pan"], correct: "pin", wordsOnly: true},
-  {words: ["lap", "lip", "loop"], correct: "lip", wordsOnly: true}, // 5
-  {words: ["sip", "sap", "soup"], correct: "sip", wordsOnly: true},
-  {words: ["boot", "beet", "bit"], correct: "bit", wordsOnly: true},
-  {words: ["spin", "spoon", "spray"], correct: "spin", wordsOnly: true},
-  {words: ["peg", "pig", "pug"], correct: "pig", wordsOnly: true},
-  {words: ["punch", "pitch", "bench"], correct: "pitch", wordsOnly: true}, // 10
-  {words: ["Ben", "Bob", "Bill"], correct: "Bill", wordsOnly: true},
-  {words: ["bed", "bad", "bid"], correct: "bid", wordsOnly: true},
-  {words: ["sleep", "sled", "slip"], correct: "slip", wordsOnly: true},
-  {words: ["pit", "pet", "putt"], correct: "pit", wordsOnly: true},
-  {words: ["pal", "pill", "pool"], correct: "pill", wordsOnly: true}, // 15
-  {words: ["chess", "chips", "cheese"], correct: "chips", wordsOnly: true},
-  {words: ["ships", "shapes", "shops"], correct: "ships", wordsOnly: true},
-  {words: ["eagle", "igloo", "ugly"], correct: "igloo", wordsOnly: true},
-  {words: ["injured", "enter", "under"], correct: "injured", wordsOnly: true}
+  {words: ["hop", "sit", "hat"], correct: "hop"},                  // 1
+  {words: ["elk", "ox", "ax"], correct: "ox", wordsOnly: true},
+  {words: ["rock", "rack", "wreck"], correct: "rock", wordsOnly: true},
+  {words: ["duck", "dock", "deck"], correct: "dock", wordsOnly: true},
+  {words: ["lock", "lick", "lake"], correct: "lock", wordsOnly: true}, // 5
+  {words: ["pop", "pipe", "pup"], correct: "pop", wordsOnly: true},
+  {words: ["pit", "pet", "pot"], correct: "pot", wordsOnly: true},
+  {words: ["Bob", "bib", "baby"], correct: "Bob", wordsOnly: true},
+  {words: ["fix", "fast", "fox"], correct: "fox", wordsOnly: true},
+  {words: ["nut", "net", "knot"], correct: "knot", wordsOnly: true}, // 10
+  {words: ["drip", "drop", "dress"], correct: "drop", wordsOnly: true},
+  {words: ["shut", "shot", "sheet"], correct: "shot", wordsOnly: true},
+  {words: ["flip", "flap", "flop"], correct: "flop", wordsOnly: true},
+  {words: ["stop", "step", "strap"], correct: "stop", wordsOnly: true},
+  {words: ["stomp", "stamp", "stump"], correct: "stomp", wordsOnly: true}, // 15
+  {words: ["top", "tip", "tape"], correct: "top", wordsOnly: true},
+  {words: ["ships", "shapes", "shops"], correct: "shops", wordsOnly: true},
+  {words: ["cot", "cut", "cat"], correct: "cot", wordsOnly: true},
+  {words: ["sax", "socks", "six"], correct: "socks", wordsOnly: true}
 ].map((activityProps) => {
   const sounds = activityProps.words.reduce((sounds, word) => {
     sounds[`teacher/${word}`] = `teacher/words/${word}`;

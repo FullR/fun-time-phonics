21bfec9ce34e64d850dd42448475c4b52acb8544
James Meyers <James.Meyers919@gmail.com>
2015-12-02 11:50:49 -0800

    Added level 63

---
+++ src/components/levels/63/index.js
@@ -0,0 +1,20 @@
+import multiConsonantReview from "components/levels/multi-consonant-review";
+
+export default multiConsonantReview({
+  title: "Review: Consonant 'b' - 'k' With Short Vowels",
+  number: 63,
+  activityData: [
+    {word: "hug", letters: ["hu", "ju", "gu"], correct: "hu"},
+    {word: "kiss", letters: ["hi", "gi", "ki"], correct: "ki", wordsOnly: true},
+    {word: "jelly", letters: ["he", "je", "ge"], correct: "je", wordsOnly: true},
+    {word: "goblin", letters: ["go", "jo", "co"], correct: "go", wordsOnly: true},
+    {word: "fish", letters: ["bi", "fi", "hi"], correct: "fi", wordsOnly: true},
+    {word: "cat", letters: ["ca", "ga", "ke"], correct: "ca", wordsOnly: true},
+    {word: "jog", letters: ["fo", "go", "jo"], correct: "jo", wordsOnly: true},
+    {word: "bed", letters: ["de", "be", "ke"], correct: "be", wordsOnly: true},
+    {word: "dump", letters: ["hu", "bu", "du"], correct: "du", wordsOnly: true},
+    {word: "gag", letters: ["ga", "ca", "ja"], correct: "ga", wordsOnly: true},
+    {word: "kid", letters: ["gi", "di", "ki"], correct: "ki", wordsOnly: true},
+    {word: "hedge", letters: ["fe", "he", "je"], correct: "he", wordsOnly: true},
+  ]
+});

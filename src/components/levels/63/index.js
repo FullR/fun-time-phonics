cac28676b48cf4ccbe64b5b25faa885983e170f1
James Meyers <James.Meyers919@gmail.com>
2015-12-02 11:55:49 -0800

    Added missing activities to level 63

--- src/components/levels/63/index.js
+++ src/components/levels/63/index.js
@@ -16,5 +16,13 @@ export default multiConsonantReview({
     {word: "gag", letters: ["ga", "ca", "ja"], correct: "ga", wordsOnly: true},
     {word: "kid", letters: ["gi", "di", "ki"], correct: "ki", wordsOnly: true},
     {word: "hedge", letters: ["fe", "he", "je"], correct: "he", wordsOnly: true},
+    {word: "judge", letters: ["ju", "gu", "du"], correct: "ju", wordsOnly: true},
+    {word: "dodge", letters: ["bo", "do", "jo"], correct: "do", wordsOnly: true},
+    {word: "fetch", letters: ["fe", "he", "je"], correct: "fe", wordsOnly: true},
+    {word: "gush", letters: ["cu", "ju", "gu"], correct: "gu", wordsOnly: true},
+    {word: "bib", letters: ["di", "bi", "ji"], correct: "bi", wordsOnly: true},
+    {word: "hitch", letters: ["fi", "ki", "hi"], correct: "hi", wordsOnly: true},
+    {word: "costume", letters: ["co", "go", "ke"], correct: "co", wordsOnly: true},
+    {word: "ketchup", letters: ["ca", "ke", "he"], correct: "ke", wordsOnly: true}
   ]
 });

6ef6fd1bee9b77d6e6e7e3b3ae697724d6e606f2
James Meyers <James.Meyers919@gmail.com>
2016-09-20 14:31:06 -0700

    Fixed sound module to use media plugin only in Android, fixed android icons, removed Mac icon from image preloading, fixed splash styling on small android tablets

--- src/images-to-preload.js
+++ src/images-to-preload.js
@@ -1,5 +1,5 @@
 // get all images except the word images (they will be loaded when needed)
-const context = require.context("../images", true, /^((?!(words)).)*$/);
+const context = require.context("../images", true, /^((?!(words)).)*\.(png|gif|ico)$/);
 const paths = context.keys().map(context);
 
 export default paths;
--- src/sound.js
+++ src/sound.js
@@ -1,7 +1,7 @@
-//if(window.device.platform === "android") {
-//  console.log("Android detected. Using native audio API");
-//  module.exports = require("./sound-mobile");
-// } else {
-//   console.log("Android not detected. Using web audio API");
-   module.exports = require("./sound-web");
-// }
+if(window.cordova && window.cordova.platformId === "android") {
+  console.log("Android detected. Using native audio API");
+  module.exports = require("./sound-mobile");
+} else {
+  console.log("Android not detected. Using web audio API");
+  module.exports = require("./sound-web");
+}
